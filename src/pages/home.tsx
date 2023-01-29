8404a0741b4a8a3adf6979332126fe3307097671
Alvin Leonardo <alvinvin00@outlook.com>
2023-01-29 12:56:13 +0700

    initial commit

---
+++ src/pages/home.tsx
@@ -0,0 +1,11 @@
+import {NextPage} from "next";
+
+const Page = (props: NextPage) => {
+    return (
+        <div>
+            <h1>Home Page</h1>
+        </div>
+    )
+}
+
+export default Page
---
+++ src/pages/index.tsx
@@ -0,0 +1,11 @@
+import {NextPage} from "next";
+
+const Page = (props: NextPage) => {
+    return (
+        <div>
+            <h1 className={"text-xl underline"}>Index Page</h1>
+        </div>
+    )
+}
+
+export default Page
