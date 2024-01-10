9f53b1d8b862f51e0d0a073a5be5b0b1ab1d2d0a
Alvin Leonardo <alvinvin00@outlook.com>
2024-01-10 14:22:40 +0700

    chore: remove unused HOC

--- src/app/[lang]/withLang.tsx
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-
-export const withLang = (Component: React.ComponentType<any>, props: object) => {
-    return ({params}: { params: { lang: string } }) => {
-        return <Component {...params} {...props} />;
-    };
-}
-
-withLang.displayName = 'withLang';
