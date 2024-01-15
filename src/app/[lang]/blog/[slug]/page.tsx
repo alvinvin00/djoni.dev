baac609c4446fd7b82ba37c09b33f2bed67dd16e
Alvin Leonardo <alvinvin00@outlook.com>
2024-01-15 09:45:25 +0700

    fix: use new method

--- src/app/[lang]/blog/[slug]/page.tsx
+++ src/app/[lang]/blog/[slug]/page.tsx
@@ -1,8 +1,5 @@
 import React from 'react';
-import * as fs from "fs";
-import matter from "gray-matter";
-import * as path from "path";
-import * as process from "process";
+import {getBlogData, getBlogs} from "@/utils/greymatter";
 
 type BlogContentProps = {
     params: {
@@ -12,32 +9,19 @@ type BlogContentProps = {
 };
 
 export const generateStaticParams = ({params: {lang}}: { params: { lang: string } }) => {
-    const blogDir = path.join(process.cwd(), `/src/assets/blog/${lang}`);
+    const blogs = getBlogs(lang);
 
-    return fs.readdirSync(`${blogDir}`).map((file) => {
-        // Read markdown file as string
-        const fullPath = path.join(blogDir, file);
-        const fileContents = fs.readFileSync(fullPath, 'utf8');
+    return blogs.map((blog) => {
+        const metadata = blog.data;
 
-        // Use gray-matter to parse the post metadata section
-        const matterResult = matter(fileContents);
-
-        return {
-            slug: matterResult.data['slug'],
-        }
-    });
-}
-
-const readBlogFileFromAssets = (lang: string, slug: string) => {
-    const filepath = path.join(process.cwd(), `/src/assets/blog/${lang}/${slug}.md`);
-
-    const fileContent = fs.readFileSync(filepath, 'utf8');
-
-    return matter(fileContent);
+        return ({
+            slug: metadata.slug
+        });
+    })
 }
 
 export const generateMetadata = ({params: {lang, slug}}: BlogContentProps) => {
-    const matterResult = readBlogFileFromAssets(lang, slug);
+    const matterResult = getBlogData(lang, slug);
 
     return {
         title: matterResult.data['title'],
@@ -47,7 +31,7 @@ export const generateMetadata = ({params: {lang, slug}}: BlogContentProps) => {
 }
 
 const Page = ({params: {lang, slug}}: BlogContentProps) => {
-    const matterResult = readBlogFileFromAssets(lang, slug);
+    const matterResult = getBlogData(lang, slug);
 
     return <>
         {matterResult.content}
