7ab11fa2e2d74218ddaacd3136a4af6b009c0186
Alvin Leonardo <alvinvin00@outlook.com>
2024-01-25 21:59:51 +0700

    feat: add changelog link

--- src/components/Layout/BetaDisclaimer.tsx
+++ src/components/Layout/BetaDisclaimer.tsx
@@ -1,22 +1,31 @@
-import {FontAwesomeIcon} from "@fortawesome/react-fontawesome";
-import {faTriangleExclamation} from "@fortawesome/free-solid-svg-icons";
-import React from "react";
-import {useTranslations} from "next-intl";
+import {faTriangleExclamation} from '@fortawesome/free-solid-svg-icons';
+import {FontAwesomeIcon} from '@fortawesome/react-fontawesome';
+import {useTranslations} from 'next-intl';
+import React from 'react';
+
 
 export const BetaDisclaimer = () => {
-    const t = useTranslations('Home')
+  const t = useTranslations('Home');
 
-    return (
-        <section
-            className={"container flex gap-2 text-sm font-bold p-2 rounded-lg bg-yellow-300 text-black my-1"}>
-            <div>
-                <FontAwesomeIcon icon={faTriangleExclamation} size={"sm"}/>
-            </div>
-            <div className={"w-auto"}>
-                <h1>
-                    {t('beta_disclaimer')}
-                </h1>
-            </div>
-        </section>
-    );
+  return (
+    <section
+      className={
+        'container flex gap-2 text-sm font-bold p-2 rounded-lg bg-yellow-300 text-black my-1'
+      }
+    >
+      <div>
+        <FontAwesomeIcon icon={faTriangleExclamation} size={'sm'} />
+      </div>
+      <div className={'w-full flex justify-between'}>
+        <h1>{t('beta_disclaimer')}</h1>
+        <a
+          href={'https://github.com/alvinvin00/djoni.dev/commits/main/'}
+          className={'underline text-blue-600 hover:text-blue-800'}
+          target={'_blank'}
+        >
+          {t('changelog_link')}
+        </a>
+      </div>
+    </section>
+  );
 };
