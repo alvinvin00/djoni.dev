1c6cb7bc6a29d71ffda324852d3af704236cb5f2
Alvin Leonardo <alvinvin00@outlook.com>
2024-05-31 22:19:33 +0700

    chore: cleanup Velite config

--- velite.config.js
+++ velite.config.js
@@ -1,4 +1,8 @@
+import {join} from 'node:path';
+
 import {defineCollection, defineConfig, s} from 'velite';
+
+const publicRootDir = join(process.cwd(), '/public');
 
 const authorable = s.object({
   author: s.string(),
@@ -9,69 +13,42 @@ const authorable = s.object({
 });
 
 const blogs = defineCollection({
-  name: 'Blog',
-  pattern: 'blog/**/*.md',
-  schema: authorable.extend({
+  name: 'Blog', pattern: 'blog/**/*.md', schema: authorable.extend({
     slug: s.slug('blog'),
     tags: s.array(s.string()).default([]),
     thumbnail: s.string(),
-  }).transform((data, {meta}) => ({
-    ...data,
-    url: meta.file.path,
-    slug: meta.file.path,
-    lang: meta.file.path.split('/')[1],
-  })),
+  }),
 });
 
 const projects = defineCollection({
-  name: 'Projects',
-  pattern: 'projects/**/*.md',
-  schema: authorable.extend({
+  name: 'Projects', pattern: 'projects/**/*.md', schema: authorable.extend({
     github: s.string().url().optional(),
-    images: s.array(s.string()).default([]),
+    images: s.array(s.image({absoluteRoot: `${publicRootDir}`})),
     link: s.string().url(),
     slug: s.slug('projects'),
     status: s.enum(['active', 'inactive', 'archived']).default('active'),
     tags: s.array(s.string()).default([]),
     thumbnail: s.string(),
-  }).transform((data, {meta}) => ({
-    ...data,
-    url: meta.file.stem,
-    slug: meta.file.path,
-    lang: meta.file.path.split('/')[1],
-  })),
+  }),
 });
 
 const about = defineCollection({
-  name: 'About',
-  pattern: `about/**/*.md`,
-  // single: true,
+  name: 'About', pattern: `about/**/*.md`, // single: true,
   schema: s.object({
-    title: s.string(),
-    date: s.string(),
-  }).transform((data, {meta}) => ({
-    ...data,
-    url: meta.file.stem,
-    lang: meta.file.path.split('/')[1],
-  })),
+    title: s.string(), date: s.string(),
+  }),
 });
 
 export const now = defineCollection({
   name: 'Now',
   pattern: `now/**/*.md`,
-  // single: true,
+  single: true,
   schema: s.object({
-    title: s.string(),
-    date: s.string(),
-  }).transform((data, {meta}) => ({
-    ...data,
-    url: meta.file.stem,
-    lang: meta.file.path.split('/')[1],
-  })),
+    title: s.string(), date: s.string(),
+  }),
 });
 
 /** @type {import('velite').UserConfig} */
 export default defineConfig({
-  root: 'src/contents',
-  collections: {about, blog: blogs, now, projects},
+  root: 'src/contents', collections: {about, blog: blogs, now, projects},
 });
