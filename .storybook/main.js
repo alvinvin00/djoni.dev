4de136c1e5c32a0d38cfa4fec4a40b1a73d01abb
Alvin Leonardo <alvinvin00@outlook.com>
2024-08-12 16:41:15 +0700

    chore: migrated Storybook config

--- .storybook/main.js
+++ .storybook/main.js
@@ -1,11 +1,21 @@
 module.exports = {
   "stories": ["../src/**/*.mdx", "../src/**/*.stories.@(js|jsx|ts|tsx)"],
-  "addons": ["@storybook/addon-links", "@storybook/addon-essentials", "@storybook/addon-interactions", "@storybook/addon-mdx-gfm"],
+  "addons": [
+    "@storybook/addon-links",
+    "@storybook/addon-essentials",
+    "@storybook/addon-interactions",
+    "@storybook/addon-mdx-gfm",
+    "@chromatic-com/storybook"
+  ],
+
   "framework": {
     name: "@storybook/nextjs",
     options: {}
   },
-  docs: {
-    autodocs: true
+
+  docs: {},
+
+  typescript: {
+    reactDocgen: "react-docgen-typescript"
   }
 };
