1d7517b2e67344f6d9b5115969d4a233a6d0bbe2
Alvin Leonardo <alvinvin00@outlook.com>
2025-09-06 15:17:55 +0700

    refactor: update Storybook addons and dependencies

--- .storybook/main.ts
+++ .storybook/main.ts
@@ -2,10 +2,8 @@ module.exports = {
   'stories': ['../src/**/*.mdx', '../src/**/*.stories.@(js|jsx|ts|tsx)'],
   'addons': [
     '@storybook/addon-links',
-    '@storybook/addon-essentials',
-    '@storybook/addon-interactions',
-    '@storybook/addon-mdx-gfm',
     '@chromatic-com/storybook',
+    '@storybook/addon-docs'
   ],
 
   'framework': {
