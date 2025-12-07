ff95bda8169deea00efe264c13a144dd513f92e8
Alvin Leonardo <alvinvin00@outlook.com>
2025-12-07 22:15:43 +0700

    feat: add nitro dependency and integrate with Vite configuration

--- vite.config.ts
+++ vite.config.ts
@@ -1,6 +1,7 @@
 import contentCollections from '@content-collections/vite';
 import {tanstackStart} from '@tanstack/react-start/plugin/vite';
 import viteReact from '@vitejs/plugin-react';
+import {nitro} from 'nitro/vite';
 import {defineConfig} from 'vite';
 import tsconfigPaths from 'vite-tsconfig-paths';
 
@@ -12,6 +13,7 @@ export default defineConfig({
     tanstackStart({
       srcDirectory: 'src', // This is the default
     }),
+    nitro(),
     viteReact(),
   ],
 });
